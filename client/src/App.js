b19f15b4b1c7f04021daf2edbb6e0b7a3776f843
Reg-Howard <reghowardwebdesign@gmail.com>
2023-06-09 16:27:56 +0100

    Loading the finished checkpoint save to ensure that the deployment error is not a syntax error and is soley related to AWS setup

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,14 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
